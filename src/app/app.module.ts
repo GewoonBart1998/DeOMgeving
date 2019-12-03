84193cf257229e7e49b51a51830fdbc0a5c11e59
BartLooij <bart.looij@gmail.com>
2019-12-03 13:51:58 +0100

    fix displaying not styled imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,7 @@ import { RegisterComponent } from './register/register.component'
 
 /* modules */
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule, MatButtonModule } from '@angular/material/';
+import { MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule } from '@angular/material/';
 
 @NgModule({
   declarations: [
@@ -20,7 +19,8 @@ import { MatIconModule, MatButtonModule } from '@angular/material/';
     BrowserAnimationsModule,
     MatInputModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
