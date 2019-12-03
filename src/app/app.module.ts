96ee08c5c5b0876cebd2ab55875e273b9f54ca1c
Ashna Wiar <Ashna_wiar@hotmail.com>
2019-12-03 13:34:24 +0100

    Added Angular Material, RegisterComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/* components */
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component'
 
+/* modules */
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule, MatButtonModule } from '@angular/material/';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
