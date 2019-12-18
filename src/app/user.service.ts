5e5f21bec48e78264eb68a39cdd656c403bea2b1
Ashna Wiar <Ashna_wiar@hotmail.com>
2019-12-18 15:28:09 +0100

    fixed error that leander pointed out

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,21 +1,21 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { User } from 'shared/user';
-import { ApiService } from './services/api.service';
+import {Injectable} from '@angular/core';
+import {User} from 'shared/user';
+import {ApiService} from './services/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private api: ApiService) { }
-
-  createUser(user:User){
-    return this.api.post('/register', user)
+  constructor(private api: ApiService) {
   }
 
-  login(user:User) {
-    return this.api.post("/login", user);
+  createUser(user: User) {
+    return this.api.post('/register', user);
+  }
+
+  login(user: User) {
+    return this.api.post('/login', user);
   }
 
 
