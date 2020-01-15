830571b3d8642b561b5bfc1fc2602e99303ade6f
Ashna Wiar <Ashna_wiar@hotmail.com>
2020-01-15 13:26:48 +0100

    added auth

--- src/app/home/page/home.component.ts
+++ src/app/home/page/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../user/shared/user.service';
+import {User} from '../../user/shared/user';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,12 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  private user: User;
 
-  loadedScreen = 'experiments';
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private userService: UserService, private router: Router) {
   }
 
+  ngOnInit() {
+    this.user = this.userService.getCurrentUser();
+    console.log(this.user);
+  }
+
+  getLoggedUserame() {
+    if (this.user) {
+      return this.user.name;
+    }
+
+    return 'onbekend';
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['/login']);
+  }
 }
