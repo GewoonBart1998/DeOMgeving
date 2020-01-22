d611ddf71639acf9a151fe9cd809da92453b5bfd
coole-guus <s1113606@student.hsleiden.nl>
2020-01-22 12:47:19 +0100

    added login on enter + removed change password feature

--- src/app/app-routing.modules.ts
+++ src/app/app-routing.modules.ts
@@ -2,10 +2,8 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/page/home.component';
 import {ManageExperimentComponent} from './home/components/manage-experiment/manage-experiment.component';
-import {ForgotPasswordComponent} from './user/page/forgot-password/forgot-password.component';
 import {UserRegisterComponent} from './user/page/user-register/user-register.component';
 import {UserLoginComponent} from './user/page/user-login/user-login.component';
-import {ChangePasswordComponent} from './user/page/change-password/change-password.component';
 import {DasboardComponent} from './home/components/dasboard/dasboard.component';
 import {AuthGuard} from './auth.guard';
 import {AdminGuard} from './admin.guard';
@@ -15,8 +13,6 @@ const appRoutes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: UserLoginComponent},
   {path: 'register', component: UserRegisterComponent},
-  {path: 'forgot-password', component: ForgotPasswordComponent},
-  {path: 'user/reset-password/:token', component: ChangePasswordComponent},
   {
     path: 'home',
     component: HomeComponent,
